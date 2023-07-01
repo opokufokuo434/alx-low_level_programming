568a9578f4f7f6b4284445b47b7ef7a82a948fc0
opokufokuo434 <opokufokuo434@yahoo.com>
2023-07-01 14:22:12 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,46 +1,34 @@
 #include "main.h"
 /**
- * _strcmp - a function that compares two strings and display
+ * _strncpy - A function that copy strings and display
  *
- * @s1: first string input to compare
- * @s2: against this other second string
+ * @dest: The pointer to destination input the buffer
+ * @src: The pointer to the source input
+ * @n: bytes of the the pointer @src
  *
- * Return: 0 if s1 and s2 are equal
- *	negative integer if the stopping character
- *		in @s1 was less than the stopping
- *		character in @s2
- *	positive integer if the stopping character
- *		in @s1 was greater than the stopping
- *		character in @s2
- */
-int _strcmp(char *s1, char *s2)
+ * Return: @dest
+*/
+char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, r;
+	int i;
 
 	/**
-	 * iterate through src and
-	 * compare it with dest
+	 * Iterate through src array
+	 * where if there is no null byte
+	 * among the first n bytes of source,
+	 * the string placed in dest will not be
+	 * null terminated
 	 */
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{
-		/**
-		 * if they differ by a single character
-		 * don't iterate further
-		 */
-		if (s1[i] != s2[i])
-		{
-			/**
-			 * return the difference between
-			 * the two characters
-			 */
-			r = s1[i] - s2[i];
-			break;
-		}
-		else
-		{
-			r = s1[i] - s2[i];
-		}
-		i++;
-	}
-	return (r);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	/**
+	 * If the length of source is less than n,
+	 * write additional null bytes to dest to
+	 * ensure that a total of n bytes is written
+	 */
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }
