0790a3b6f2de8946b3f6de3dc393152d2b045f84
opokufokuo434 <opokufokuo434@yahoo.com>
2023-07-21 11:35:55 -0700

    3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,23 +1,57 @@
-#ifndef _CALC_H
-#define _CALC_H
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h> /*for atoi*/
+#include <stdlib.h> /*for exit*/
 
 /**
- * struct op - Struct op
+ * main - Program Entry
  *
- * @op: The operator
- * @f: The function associated
+ * Description: Program that takes 4 arguments where 2 are
+ *              int variables plus an operator to perform
+ *              a function between the two numbers
+ *
+ *              Usage: ./calc num1 operator num2
+ *                         |     |       |     |_ argv[3]
+ *                         |     |       |_ argv[2]
+ *                         |     |_ argv[1]
+ *                         |_ argv[0]
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0-on success
  */
-typedef struct op
+int main(int argc, char *argv[])
 {
-	char *op;	/* The operator */
-	int (*f)(int a, int b);	/* The function associated */
-} op_t;
+	int num1, num2;
+	char *operator;
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+	/* Error if arguments are not 4 with the executable */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-#endif /* _CALC_H */
+	num1 = atoi(argv[1]); /*if argument is string convert to number*/
+	num2 = atoi(argv[3]); /* as stated above */
+	operator = argv[2];
+
+	/* Error if operator is not null or a 2d array '\*' */
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	/* Error if user tries to divide / (47) or % (37) by 0*/
+	if ((*operator == 47 || *operator == 37) && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(operator)(num1, num2));
+
+	return (0);
+}
