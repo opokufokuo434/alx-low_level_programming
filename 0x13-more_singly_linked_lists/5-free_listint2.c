42f531c87cea1fbc6fafe18d58f59a50e5616a56
opokufokuo434 <opokufokuo434@yahoo.com>
2023-08-02 09:58:28 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,32 +1,35 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
-	* free_listint2 - Frees a list and sets its head to NULL
-	*
-	* @head: double pointer to the head of the list
-	*
-	* Return: Nothing
-	* Description:
-	*	This function frees the memory occupied by each node in the linked list
-	*	starting from the head. It also sets the head of the list to NULL,
-	*	ensuring that the list is empty after freeing all nodes.
-	*/
+ * free_listint2 - Frees the list and sets head to NULL.
+ *
+ * @head: Pointer to a pointer to the first node.
+ *
+ * Description:
+ * The function takes a pointer to a pointer to the first node linked list
+ * It frees the memory occupied by each node in the list
+ * to NULL to ensure it no longer points to the freed memory.
+ */
 void free_listint2(listint_t **head)
 {
-	listint_t *Nodetemp, *currNode;
+	listint_t *current_node, *temp_node;
 
-	if (head == NULL || *head == NULL)
-		return;
-
-	currNode = *head;
-
-	/* for looping for better readability */
-	for (; currNode != NULL; currNode = currNode->next)
+	if (head != NULL)
 	{
-		Nodetemp = currNode;
-		free(Nodetemp);
+	/* Set current_node to point to the first node of the linked list */
+	current_node = *head;
+
+	/* Iterate through the whole linked list */
+	for (temp_node = current_node; temp_node != NULL; temp_node = current_node)
+	{
+	/* Move current_node to the next node before freeing temp_node */
+	current_node = current_node->next;
+
+	/* Free the memory occupied by the current_node (temp_node) */
+	free(temp_node);
 	}
 
+	/* After the loop, all nodes have been freed, so set head to NULL */
 	*head = NULL;
+	}
 }
